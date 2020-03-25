a75fbea5b9931f83d70c848d100316913953d817
Jayaleshwari N <jayaleshwarin@syncfusion.com>
2020-03-25 12:34:17 +0530

    XAMARIN-34085 - Sample added.

---
+++ ListViewXamarin/ListViewXamarin/ListViewXamarin/Model/Contacts.cs
@@ -0,0 +1,65 @@
+﻿using FreshMvvm;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ListViewXamarin
+{
+    public class Contacts : FreshBasePageModel
+    {
+        private string contactName;
+        private string contactNumber;
+        private ImageSource image;
+
+        public Contacts(string name, string number)
+        {
+            contactName = name;
+            contactNumber = number;
+        }
+
+        public Contacts()
+        {
+
+        }
+
+        public string ContactName
+        {
+            get { return contactName; }
+            set
+            {
+                if (contactName != value)
+                {
+                    contactName = value;
+                    this.RaisePropertyChanged();
+                }
+            }
+        }
+
+        public string ContactNumber
+        {
+            get { return contactNumber; }
+            set
+            {
+                if (contactNumber != value)
+                {
+                    contactNumber = value;
+                    this.RaisePropertyChanged();
+                }
+            }
+        }
+
+        public ImageSource ContactImage
+        {
+            get { return this.image; }
+            set
+            {
+                this.image = value;
+                this.RaisePropertyChanged();
+            }
+        }
+    }
+}
---
+++ ListViewXamarin/ListViewXamarin/ListViewXamarin/Views/ListViewPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using Syncfusion.DataSource;
+using Syncfusion.ListView.XForms;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ListViewXamarin
+{
+    public partial class ListViewPage : ContentPage
+    {
+        #region Constructor
+        public ListViewPage()
+        {
+            InitializeComponent();
+        }
+        #endregion
+    }
+}
